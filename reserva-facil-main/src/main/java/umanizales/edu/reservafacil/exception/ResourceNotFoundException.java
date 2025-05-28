2dbe6f5818a6186e7a700a8ee20a1a37d68c3a9b
Sebastian Norabuena Jimenez <sebastiannorabuena15@gmail.com>
2025-05-27 22:12:33 -0500

    agregar controlador de errores

---
+++ reserva-facil-main/src/main/java/umanizales/edu/reservafacil/exception/ResourceNotFoundException.java
@@ -0,0 +1,11 @@
+package umanizales.edu.reservafacil.exception;
+
+public class ResourceNotFoundException extends RuntimeException{
+	public ResourceNotFoundException() {
+		super();
+	}
+
+	public ResourceNotFoundException(String message) {
+		super(message);
+	}
+}
