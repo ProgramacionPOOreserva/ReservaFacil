00f31b835263e439b8cb3356c6728859f9517acd
Sebastian Norabuena Jimenez <sebastiannorabuena15@gmail.com>
2025-05-27 22:07:56 -0500

    First Commit

---
+++ reserva-facil-main/src/test/java/umanizales/edu/reservafacil/ReservafacilApplicationTests.java
@@ -0,0 +1,13 @@
+package umanizales.edu.reservafacil;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReservafacilApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
